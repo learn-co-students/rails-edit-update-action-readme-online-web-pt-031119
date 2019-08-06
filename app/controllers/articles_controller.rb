e6f18a83f04aac2b86dc58572e4904132752564a
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-08-06 16:09:44 +0000

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,6 +18,12 @@ class ArticlesController < ApplicationController
     @article.save
     redirect_to article_path(@article)
   end
-
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+ 
+  def update
+  end
   # add edit and update methods here
 end
